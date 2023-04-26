ccc453ce283a6b5dec2f611f5d45a164ea454d40
pradeep <pradeeprakh2001@gmail.com>
2023-04-26 18:52:19 +0530

    add two number

--- EVEN-ODD.js
+++ EVEN-ODD.js
@@ -1,7 +1,7 @@
 // program to check if the number is even or odd
 const prompt=require('prompt-sync')()
-number = prompt("Enter a number: ");
-if(number % 2 == 0) {
+num = parseInt(prompt("Enter a number: "))
+if(num % 2 == 0) {
     console.log("The number is even.");
 }
 
